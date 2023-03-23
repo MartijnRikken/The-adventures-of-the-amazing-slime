169695a637f4f503bee279400fa5d205c5b14d9a
MartijnRikken <60739643+MartijnRikken@users.noreply.github.com>
2023-03-23 17:53:06 +0100

    m

---
+++ src/main/java/com/github/hanyaeger/tutorial/entities/TrafficLight/Licht.java
@@ -0,0 +1,16 @@
+package com.github.hanyaeger.tutorial.entities.TrafficLight;
+
+import com.github.hanyaeger.api.Coordinate2D;
+import com.github.hanyaeger.api.Size;
+import com.github.hanyaeger.api.entities.DynamicCompositeEntity;
+import com.github.hanyaeger.api.entities.impl.DynamicSpriteEntity;
+
+public class Licht extends DynamicSpriteEntity {
+
+    boolean switchen;
+
+    public Licht(Coordinate2D initialLocation, int columns) {
+        super("",initialLocation, new Size(200, 200), 1, 2);
+        setCurrentFrameIndex(columns);
+    }
+}
