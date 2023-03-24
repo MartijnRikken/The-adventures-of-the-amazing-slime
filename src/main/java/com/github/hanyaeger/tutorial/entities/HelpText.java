efde17b13ea5c028e51e9521705055cdf7e62d44
MartijnRikken <60739643+MartijnRikken@users.noreply.github.com>
2023-03-24 10:44:26 +0100

    m

--- src/main/java/com/github/hanyaeger/tutorial/entities/HelpText.java
+++ src/main/java/com/github/hanyaeger/tutorial/entities/HelpText.java
@@ -7,20 +7,19 @@ import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 
 public class HelpText extends TextEntity {
-    public HelpText(Coordinate2D initialLocation, int actief) {
-        super(initialLocation);
-        if (actief == 1){
-            setText("look at the notes");
-            setFill(Color.LIGHTBLUE);
-            setFont(Font.font("Roboto", FontWeight.BOLD, 30));
 
-            setText("but don't go to fast for one miscast");
-            setAnchorLocation(new Coordinate2D(30, 0));
+    ActiveScene scherm;
+    public HelpText(Coordinate2D initialLocation, ActiveScene scherm) {
+        super(initialLocation);
+        if (scherm.oldScene == 1){
+            setText("look at the notes but don't go to fast for one miscast");
+            setFill(Color.LIGHTBLUE);
+            setFont(Font.font("Roboto", FontWeight.BOLD, 20));
         }
-        else if (actief == 2){
+        else if (scherm.oldScene == 2){
             setText("you can go on red but don't be sad for red will hurt but green is clean");
             setFill(Color.LIGHTBLUE);
-            setFont(Font.font("Roboto", FontWeight.BOLD, 30));
+            setFont(Font.font("Roboto", FontWeight.BOLD, 20));
         }
     }
 }
